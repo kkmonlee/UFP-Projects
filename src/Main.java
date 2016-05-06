98d0d944fb6d3672c0cb644643d7c2e6a96ca365
kkmonlee <kkmonlee@gmail.com>
2016-05-06 19:31:34 +0100

    Final changes

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
         Task theTask = new Task();
 
-        System.out.println("Enter the number you want the square root of: ");
-        Scanner input = new Scanner(System.in);
-        System.out.println(theTask.HerosMethod(input.nextDouble()));
+        theTask.HerosMethod();
 
     }
 }
