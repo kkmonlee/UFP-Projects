65db48679b1d6e33a4c5663e389f835c4d058129
kkmonlee <kkmonlee@gmail.com>
2016-05-25 01:57:49 +0100

    Topic addition

---
+++ Data Structures/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Atul Anand Sinha on 25 May 2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ Data Structures/src/Stack.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Atul Anand Sinha on 25 May 2016.
+ */
+public class Stack {
+    public Stack() {
+        final int capacity = 5;
+        int arr[] = new int[capacity];
+        int pointer;
+        pointer = -1;
+    }
+}
